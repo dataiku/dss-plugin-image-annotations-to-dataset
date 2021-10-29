32c81e7b3315d21b35dbe618a6dde063c54fdf8e
Claire <claire.behue@dataiku.com>
2021-10-29 18:23:06 +0200

    cleaning & refactoring

--- custom-recipes/dataset-annotations-formatter-recipe/recipe.py
+++ custom-recipes/dataset-annotations-formatter-recipe/recipe.py
@@ -2,42 +2,24 @@
 
 import logging
 import dataiku
-import json
-
 from dataiku.customrecipe import get_recipe_config, get_input_names_for_role, get_output_names_for_role
 
+from lib import format_labelling_plugin_annotations
 
-def format_annotations(image_annotations):
-    image_annotations = json.loads(image_annotations)
-    deephub_image_annotations = []
-    for annotation in image_annotations:
-        deephub_image_annotations.append(
-            {"bbox": [annotation.get("left"), annotation.get("top"), annotation.get("width"), annotation.get("height")],
-             "area": annotation.get("height") * annotation.get("width"),
-             "iscrowd": False,
-             "category": annotation.get("label")
-             })
-    image_annotations = json.dumps(deephub_image_annotations)
-    return image_annotations
+if __name__ == '__main__':
+    input_dataset = dataiku.Dataset(get_input_names_for_role("input_dataset")[0])
 
+    # copy all the columns from input dataset (only target one will be changed)
+    output_df = input_dataset.get_dataframe()
 
-input_dataset = dataiku.Dataset(get_input_names_for_role("input_dataset")[0])
-output_dataset = dataiku.Dataset(get_output_names_for_role("output_dataset")[0])
-parameters = get_recipe_config()
+    target_column = get_recipe_config()["target_column"]
+    if target_column not in output_df.columns:
+        raise Exception("Column {} not in input_df with columns [{}]".format(target_column, list(output_df.columns)))
 
-# copy all the columns from input dataset (only target one will be changed)
-output_df = input_dataset.get_dataframe()
-target_column = parameters.get("target_column")
-if target_column not in output_df.columns:
-    logging.error("column {} not in input df columns {}".format(target_column, output_df.columns))
-    raise Exception("column {} not in input df {}".format(target_column, output_df))
+    logging.info("Input format of data: ML-assisted-labelling-plugin")
+    logging.info("Annotations target column chosen: {}".format(target_column))
 
-logging.info("ML-assisted-labelling-plugin")
-logging.info(target_column)
-logging.info(output_df[target_column].head())
-output_df = output_df
-output_df[target_column] = output_df[target_column].map(format_annotations)
-logging.info("after change:")
-logging.info(output_df[target_column].head())
+    output_df[target_column] = output_df[target_column].map(format_labelling_plugin_annotations)
 
-output_dataset.write_with_schema(output_df)
+    output_dataset = dataiku.Dataset(get_output_names_for_role("output_dataset")[0])
+    output_dataset.write_with_schema(output_df)
--- custom-recipes/image-annotations-to-dataset-recipe/recipe.py
+++ custom-recipes/image-annotations-to-dataset-recipe/recipe.py
@@ -1,99 +1,28 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from collections import defaultdict
-
-import pandas as pd
-import os
 import dataiku
-import xml.etree.ElementTree as ET
-
-import json
 
 from dataiku.customrecipe import get_recipe_config, get_input_names_for_role, get_output_names_for_role
+from lib import create_dataset_df_from_coco_json_file, create_dataset_df_from_voc_files
 
+if __name__ == '__main__':
+    input_folder = dataiku.Folder(get_input_names_for_role("input_folder")[0])
+    parameters = get_recipe_config()
+    input_data_format, images_folder_path = parameters["input_data_format"], parameters["images_folder_path"]
 
-def retrieve_annotations_from_xml_file(annotation_file_stream):
-    image_annotations = []
-    tree = ET.parse(annotation_file_stream)
-    root = tree.getroot()
+    if input_data_format == "coco":
+        logging.info("Input format of data: COCO format")
 
-    for o in root.iter('object'):
-        difficult = int(o.find('difficult').text == '1')
-        category = o.find('name').text.lower().strip()
+        coco_json_file = input_folder.read_json(parameters["coco_json_filepath"])
+        output_df = create_dataset_df_from_coco_json_file(coco_json_file, images_folder_path)
 
-        bbox = o.find('bndbox')
-        xmin = int(bbox.find('xmin').text) - 1
-        ymin = int(bbox.find('ymin').text) - 1
-        xmax = int(bbox.find('xmax').text) - 1
-        ymax = int(bbox.find('ymax').text) - 1
+    elif input_data_format == "voc":
+        logging.info("Input format of data: VOC pascal format")
+        output_df = create_dataset_df_from_voc_files(input_folder, images_folder_path, parameters["annotations_folder_path"])
 
-        height = ymax - ymin
-        width = xmax - xmin
+    else:
+        raise Exception("Input format unknown: {}".format(input_data_format))
 
-        image_annotations.append(
-            {"bbox": [xmin, ymin, width, height],
-             "area": height * width,
-             "iscrowd": False,
-             "category": category,
-             "difficult": difficult}
-        )
-    return image_annotations
-
-
-input_folder = dataiku.Folder(get_input_names_for_role("input_folder")[0])
-output_dataset = dataiku.Dataset(get_output_names_for_role("output_dataset")[0])
-parameters = get_recipe_config()
-input_data_format = parameters.get("input_data_format")
-images_folder_path = parameters.get("images_folder_path")
-
-if input_data_format == "coco":
-    logging.info("COCO format")
-
-    annotations_file_content = input_folder.read_json(parameters.get("annotations_file_path"))
-
-    # build intermediate dicts to facilitate formatting:
-    images_id_to_path = {img.get("id"): os.path.join(images_folder_path, img.get("file_name"))
-                         for img in annotations_file_content.get("images")}
-    category_id_to_name = {cat.get("id"): cat.get("name") for cat in annotations_file_content.get("categories")}
-
-    single_annotations = annotations_file_content.get("annotations")
-    annotations_per_img = defaultdict(list)
-    for single_annotation in single_annotations:
-        # add category name to annotation dict
-        single_annotation["category"] = category_id_to_name.get(single_annotation.get("category_id"))
-
-        # a single image can have multiple annotations, add this one to the list (create a new list if needed)
-        img_id = single_annotation.pop("image_id")
-        annotations_per_img[img_id].append(single_annotation)
-
-    output_df = pd.DataFrame([{"images_annotations": json.dumps(annotations_per_img[img_id]),
-                               "images_path": img_path}
-                              for img_id, img_path in images_id_to_path.items()])
-
-elif input_data_format == "voc":
-    logging.info("VOC pascal format")
-
-    images_basename_to_fullpath = {os.path.splitext(img.get("name"))[0]: img.get("fullPath")
-                                   for img in input_folder.get_path_details(images_folder_path).get("children")}
-
-    output_list = []
-    # loop over annotations files from annotation folder:
-    for image_annotations_file in input_folder.get_path_details(parameters.get("annotations_folder_path")).get("children"):
-        xml_fullpath = image_annotations_file.get("fullPath")
-        image_basename = os.path.splitext(image_annotations_file.get("name"))[0]
-        if image_basename not in images_basename_to_fullpath:
-            raise Exception("No image file corresponding to annotations file {} in folder {}".format(
-                xml_fullpath, images_folder_path
-            ))
-        with input_folder.get_download_stream(xml_fullpath) as annotations_file_stream:
-            output_list.append({
-                "images_annotations": json.dumps(retrieve_annotations_from_xml_file(annotations_file_stream)),
-                "images_path": images_basename_to_fullpath.get(image_basename)
-            })
-    output_df = pd.DataFrame(output_list)
-
-else:
-    raise Exception("Input format unknown: {}".format(input_data_format))
-
-output_dataset.write_with_schema(output_df)
+    output_dataset = dataiku.Dataset(get_output_names_for_role("output_dataset")[0])
+    output_dataset.write_with_schema(output_df)
