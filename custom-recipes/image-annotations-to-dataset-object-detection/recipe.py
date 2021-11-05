9bb2c5ae9b458de42dde6bcca4ce82bc57e0d4fc
Claire <claire.behue@dataiku.com>
2021-11-05 16:55:17 +0100

    add another dummy commit

--- custom-recipes/image-annotations-to-dataset-object-detection/recipe.py
+++ custom-recipes/image-annotations-to-dataset-object-detection/recipe.py
@@ -10,18 +10,18 @@ parameters = get_recipe_config()
 input_data_format, images_folder_path = parameters["input_data_format"], parameters["images_folder_path"]
 
 if input_data_format == "coco":
-    logging.info("Input format of data: COCO format")
+    logging.info("Annotations format: COCO format")
 
     coco_json_file = input_folder.read_json(parameters["coco_json_filepath"])
     output_df = create_dataset_df_from_coco_json_file(coco_json_file, images_folder_path)
 
 elif input_data_format == "voc":
-    logging.info("Input format of data: VOC pascal format")
+    logging.info("Annotations format: VOC pascal format")
     output_df = create_dataset_df_from_voc_files(input_folder, images_folder_path,
                                                  parameters["annotations_folder_path"])
 
 else:
-    raise Exception("Input format unknown: {}".format(input_data_format))
+    raise Exception("Annotations format unknown: {}".format(input_data_format))
 
 output_dataset = dataiku.Dataset(get_output_names_for_role("output_dataset")[0])
 output_dataset.write_with_schema(output_df)
